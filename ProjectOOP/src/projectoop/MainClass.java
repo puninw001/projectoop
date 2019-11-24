49adc132a4d09cf71343e93122b444777376993b
Runchapol <43022331+Runchapol@users.noreply.github.com>
2019-11-24 17:14:04 +0700

    Update MainClass.java

--- ProjectOOP/src/projectoop/MainClass.java
+++ ProjectOOP/src/projectoop/MainClass.java
@@ -4,7 +4,6 @@ public class MainClass {
 
     public static final int WIDTH=640;
     public static final int HEIGHT=480;
-    int Ly = 160;
     public static void main(String[] args) {
         new Menu();
     }
