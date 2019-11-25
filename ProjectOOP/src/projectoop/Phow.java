41e2340f1cbb398490667a01c1c534caba583731
Runchapol <43022331+Runchapol@users.noreply.github.com>
2019-11-25 19:14:19 +0700

    Update Phow.java

--- ProjectOOP/src/projectoop/Phow.java
+++ ProjectOOP/src/projectoop/Phow.java
@@ -15,7 +15,7 @@ import java.io.File;
 import javax.swing.*;
 import javax.swing.border.*;
 public class Phow extends JPanel{
-    int px = 120, py = 50;
+    int px = 120, py = 30;
     Image bg = Toolkit.getDefaultToolkit().createImage(System.getProperty(
           "user.dir")+File.separator+"img"+File.separator+"bhow.png");
     Image th = Toolkit.getDefaultToolkit().createImage(System.getProperty(
@@ -28,13 +28,14 @@ public class Phow extends JPanel{
     @Override
     public void paintComponent(Graphics g){
         g.drawImage(bg, 0, 0,640,480,0,0,640,480, this);
-        g.drawImage(th, px, py,px+400,py+70,0,0,400,65, this);
+        g.drawImage(th, px, py,px+400,py+90,0,0,400,65, this);
         g.setFont(new Font("Angsana New", Font.BOLD, 30));
         g.setColor(Color.yellow);
         g.drawString("กดปุ่มลูกศรขึ้น (^) เพื่อเคลื่อนที่ขึ้น", px+60, py+120);
         g.drawString("กดปุ่มลูกศรลง (v) เพื่อเคลื่อนที่ลง", px+60, py+160);
         g.drawString("กดปุ่มลูกศรซ้าย (<-) เพื่อเคลื่อนไปทางซ้าย", px+40, py+200);
         g.drawString("กดปุ่มลูกศรขวา (->) เพื่อเคลื่อนไปทางขวา", px+40, py+240);
-        g.drawString("หากตัวเราชนกับศัตรู ก็ Game Over", px+50, py+280);
+        g.drawString("หากตัวเราชนกับศัตรู จะตัด Score เป็น 0 แต่เล่นต่อได้", px, py+280);
+        g.drawString("เพราะการลงทัณฑ์มันไม่มีที่สิ้นสุด!", px+60, py+320);
     }
 }
