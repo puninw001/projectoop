d975851e4b2d7f14a1edaef368eaf3cda4fa0dc4
Runchapol <43022331+Runchapol@users.noreply.github.com>
2019-11-25 20:51:00 +0700

    Update Game.java

--- ProjectOOP/src/projectoop/Game.java
+++ ProjectOOP/src/projectoop/Game.java
@@ -7,7 +7,11 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
@@ -18,6 +22,7 @@ public class Game extends JPanel implements ActionListener{
     Player player;
     public static int score = 0;
     ControlEnemy c;
+    private int hsc = 0;
     public Game() {
         loop = new Timer(10, this);
         loop.start();
@@ -47,11 +52,34 @@ public class Game extends JPanel implements ActionListener{
         g2d.setColor(Color.white);
         g2d.setFont(new Font("Tahoma",Font.BOLD,20));
         g2d.drawString("Score : "+score,0,20);
+        File f = new File("highscore.dat");
+        FileOutputStream fout;
+        ObjectOutputStream oout;
+        FileInputStream fin;
+        ObjectInputStream oin;
+        try{
+            fin = new FileInputStream("highscore.dat");
+            oin = new ObjectInputStream(fin);
+            if(f.exists()){
+                hsc = (int) oin.readObject();
+            }
+            oin.close();
+            fin.close();
+        }
+        catch(IOException e){
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        g2d.drawString("High Score : "+hsc,0,50);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        player.update();
+        try {
+            player.update();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
         c.update();
         score++;
         repaint();
